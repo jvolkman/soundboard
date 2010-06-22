a3af558281e09107c95a8166151cae2008200444
Jeremy Volkman <jvolkman@gmail.com>
2010-06-21 22:49:04 -0400

    updated to use relayout plugin

--- public/javascripts/jquery.govertical.js
+++ public/javascripts/jquery.govertical.js
@@ -4,25 +4,36 @@
  * 
  * divs will be tried in order. The first one wins.
  */
-jQuery.fn.govertical = function(boundaries) {
-    if (boundaries) {
-        if (!(boundaries instanceof Array)) {
-            boundaries = [boundaries];
-            $(this).data("govertical.boundaries", boundaries);
-        }
-    }
-    this.each(function(){
-        var boundaries = $(this).data("govertical.boundaries");
-        if (boundaries) {
-            var from = $(this).offset().top + $(this).outerHeight(false) + parseInt($(this).css("margin-bottom"));
-            for (var i in boundaries) {
-                var el = $(boundaries[i]);
-                if (el.is(":visible")) {
-                    var to = el.offset().top;
-                    $(this).height($(this).height() + (to - from));
-                    break;
-                }
+(function($){
+/* takes a jQuery object */
+function doit(boundaries) {
+    return this.each(function() {
+        var $this = $(this);
+        var from = $this.offset().top + $this.outerHeight(false) + parseInt($this.css("margin-bottom"));
+        for (var i in boundaries) {
+            var el = $(boundaries[i]);
+            if (el.is(":visible")) {
+                var to = el.offset().top - parseInt(el.css("margin-top"));
+                $this.height($this.height() + (to - from));
+                break;
             }
         }
     });
+}
+
+$.fn.govertical = function(boundaries) {
+    if (boundaries) {
+        if (!(boundaries instanceof Array)) {
+            boundaries = [boundaries];
+        }
+        if ($.fn.relayout) {
+            this.each(function() {
+                $(this).relayout("govertical", function() {
+                    doit.call($(this), boundaries);
+                });
+            });
+        }
+        return doit.call(this, boundaries);
+    }
 };
+})(jQuery);
