98186683e158e028860652db5bfe466c9b62db91
Jeremy Volkman <jvolkman@gmail.com>
2010-06-22 11:19:57 -0400

    Makes govertical more efficient

--- public/javascripts/jquery.govertical.js
+++ public/javascripts/jquery.govertical.js
@@ -5,10 +5,8 @@
  * 
  * jQuery plugin which stretches a div to reach another div below it.
  * Usage:
- *   $(selector).goVertical({boundary: div});
- * OR 
- *   $(selector).goVertical({boundary: [div1, div2, ...]}) 
- *   (divs will be tried in order. The first one wins.)
+ *   $(selector).goVertical({boundary: <selector>});
+ *   (divs will be tried in selection order. The first one wins.)
  * 
  * Options: 
  *   includeMargins: include margins in calculations (default: true)
@@ -17,34 +15,35 @@
 (function($){
 /* this: a jQuery object */
 function doit(args) {
-    return this.each(function() {
+    var $boundary = $(args.boundary);
+    var from = this.offset().top + this.outerHeight(false) + (args.includeMargins ? parseInt(this.css("margin-bottom")) : 0);
+    var $subject = this;
+    $(args.boundary).each(function() {
         var $this = $(this);
-        args = $.extend({}, $.fn.goVertical.defaults, args);
-        if ($.metadata) {
-            $.extend(args, $this.metadata());
+        if ($this.is(":visible")) {
+            var to = $this.offset().top - (args.includeMargins ? parseInt($this.css("margin-top")) : 0);
+            $subject.height($subject.height() + (to - from));
+            return false;
         }
-        if (args && args.boundary) {
-            var boundary = args.boundary instanceof Array ? args.boundary : [args.boundary];
-            var from = $this.offset().top + $this.outerHeight(false) + (args.includeMargins ? parseInt($this.css("margin-bottom")) : 0);
-            for (var i in boundary) {
-                var el = $(boundary[i]);
-                if (el.is(":visible")) {
-                    var to = el.offset().top - (args.includeMargins ? parseInt(el.css("margin-top")) : 0);
-                    $this.height($this.height() + (to - from));
-                    break;
-                }
-            }
-        }            
     });
 };
 
 $.fn.goVertical = function(args) {
-    if ($.relayout) {
-        this.relayout("govertical", function() {
-            doit.call($(this), args);
-        });
-    }
-    return doit.call(this, args);
+    args = $.extend({}, $.fn.goVertical.defaults, args);
+	return this.each(function() {
+		var $this = $(this);
+		if ($.metadata) {
+            args = $.extend({}, args, $this.metadata());
+        }
+		if (args.boundary) {
+			if ($.relayout) {
+		        $this.relayout("govertical", function() {
+		            doit.call($(this), args);
+		        });
+		    }
+		    return doit.call($this, args);
+		}
+	});
 };
 
 $.fn.goVertical.defaults = {
